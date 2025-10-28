1f8559b25f4f64a92117b264bfe49029c7119829
이지우 <dlwldn041020@hufs.ac.kr>
2025-10-28 15:24:57 +0900

    feat: 헤더 ui 및 네비게이션 기능 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Logo from "../assets/logo.svg";
 import GlobalStyle  from '../styles/GlobalStyle';
+import { useNavigate } from "react-router-dom";
 
 const Container = styled.div`
   width: 100vw;
@@ -29,21 +30,24 @@ const MenuItem = styled.div`
   font-weight: 400;
   line-height: 140%;
   letter-spacing: -0.0125rem;
+  cursor: pointer;
 `
 
 
 export default function Header() {
-  
+
+  const navigate = useNavigate();
+
   return(
     <Container>
       <HeaderLogo src={Logo} alt="logo"/>
       <Menu>
-        <MenuItem>홈</MenuItem>
-        <MenuItem>랜덤 매칭</MenuItem>
-        <MenuItem>스터디 모집</MenuItem>
-        <MenuItem>프로필 조회</MenuItem>
-        <MenuItem>대화방</MenuItem>
-        <MenuItem>MYPAGE</MenuItem>
+        <MenuItem onClick={() => navigate("/")}>홈</MenuItem>
+        <MenuItem onClick={() => navigate("/random-match")}>랜덤 매칭</MenuItem>
+        <MenuItem onClick={() => navigate("/study")}>스터디 모집</MenuItem>
+        <MenuItem onClick={() => navigate("/profile")}>프로필 조회</MenuItem>
+        <MenuItem onClick={() => navigate("/message")}>대화방</MenuItem>
+        <MenuItem onClick={() => navigate("/mypage")}>MYPAGE</MenuItem>
       </Menu>
     </Container>
   )
