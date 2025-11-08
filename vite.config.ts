4950c3ae01ee77e4891cbd61360f0f1b55caa73b
이지우 <dlwldn041020@hufs.ac.kr>
2025-11-08 20:55:33 +0900

    Chore: vite.config.ts 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: 'localhost',
+    port: 5174, // 👈 여기가 핵심
+    hmr: {
+      host: 'localhost',
+      port: 5174, // 👈 HMR도 동일 포트로 맞추기
+      protocol: 'ws',
+    },
+  },
 })
